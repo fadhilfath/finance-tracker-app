807404498f0784867fa584ecbe77070e9f6ca4a1
fadhilsuhendi <fadhilfathurrahman@rocketmail.com>
2016-02-24 18:56:18 +0000

    added devise and bootstrap for authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  
+  devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -6,6 +8,7 @@ Rails.application.routes.draw do
   # root 'welcome#index'
   
   root 'welcome#index'
+  
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
